ab89eb27e0588a5c38672bdadb0f9c2750b719a0
User <greatpeace@@yandex.ru>
2026-03-11 11:43:04 +0300

    add file parking_app/tests/factories.py

---
+++ module_30_ci_linters/parking_app/tests/factories.py
@@ -0,0 +1,46 @@
+import random
+
+import factory
+from factory.faker import Faker
+
+from app import db
+from app.models import Client, Parking
+
+
+class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Client
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = "commit"
+
+    name = Faker("first_name", locale="ru_RU")
+
+    surname = Faker("last_name", locale="ru_RU")
+
+    credit_card = factory.Maybe(
+        "has_credit_card",
+        yes_declaration=Faker("credit_card_number", card_type="visa"),
+        no_declaration=None,
+    )
+
+    car_number = factory.Sequence(lambda n: f"CAR{n:03d}")
+
+    class Params:
+        has_credit_card = factory.Trait(
+            credit_card=factory.Faker("credit_card_number", card_type="visa")
+        )
+
+
+class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Parking
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = "commit"
+
+    address = Faker("address", locale="ru_RU")
+
+    opened = Faker("boolean", chance_of_getting_true=70)
+
+    count_places = Faker("random_int", min=10, max=200)
+
+    count_available_places = factory.LazyAttribute(lambda o: random.randint(0, o.count_places))
